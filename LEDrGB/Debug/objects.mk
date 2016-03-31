afac19695821b3fdd713ede9c8355510ac3e988d
JakeChenCCB <Jake.Chen@nexpaq.com>
2016-03-31 17:00:35 +0800

    Modify the LED_rGB project file and add .gitignore

--- LEDrGB/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"D:/Carlota/3.\ Firmware/2.FW_V2/simple\ examples/LEDrGB/LEDrGB/mdk/np_module_mdk_v2.lib" -l"libc.a"
-
