d50ab4d60c7768d7dc963b9b245dd307db14a155
Carlota <carlota.sentis@nexpaq.com>
2016-08-30 14:08:27 -0700

    tiddy respostory

--- example project/2016-05-19/np_module_v1_example/Debug/tmain.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-tmain.obj: ../tmain.cpp
-tmain.obj: D:/GitHub/np-private-sw-firmware-msp430g2553-modules/NCN_module/np_module_v1_example/mdk/np_module_mdk_v1.h
-tmain.obj: ../t_my_app.h
-
-../tmain.cpp: 
-D:/GitHub/np-private-sw-firmware-msp430g2553-modules/NCN_module/np_module_v1_example/mdk/np_module_mdk_v1.h: 
-../t_my_app.h: 
