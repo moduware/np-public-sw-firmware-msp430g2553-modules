9f2c8069a9eb6bf06588d8b35eff3669d091baec
Alan Lin <alan.lin@nexpaq.com>
2016-11-29 12:23:08 +0800

    Module example project version update: 0.4.5       Module MDK version update: 1.0.6
    Module example project version update: 0.4.5       Module MDK version
    update: 1.0.6

---
+++ module example project/2016-05-19 Version_0.4.2/np_module_v1_example/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := $(GEN_CMDS__FLAG) -l"D:/GitHub/np-private-sw-firmware-msp430g2553-modules/NCN_module/np_module_v1_example/mdk/np_module_mdk_v1.lib" -l"libc.a"
+
